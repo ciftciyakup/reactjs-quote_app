0b0a66998c363bb59dfaf553f559b0d23c3b53f5
Yakup Ciftci <103890221+ciftciyakup@users.noreply.github.com>
2024-02-25 22:43:23 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+// Bootstrap Classes
+const appClass = "card m-3";
+const cardHeaderClass = "card-header";
+const cardBodyClass = "card-body";
+const blockQuoteClass = "blockquote mb-0";
+const footerClass = "blockquote-footer";
+const buttonClass = "btn btn-primary";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const getQuote = () =>
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => setQuote(data[Math.round(Math.random() * data.length)]));
+  useEffect(() => {
+    getQuote();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={appClass}>
+      <div className={cardHeaderClass}>Alıntı</div>
+      <div className={cardBodyClass}>
+        <blockquote className={blockQuoteClass}>
+          <p>{quote.text}</p>
+          <footer className={footerClass}>
+            <cite>{quote.author}</cite>
+          </footer>
+          <button className={buttonClass} onClick={getQuote}>
+            Yeni Alıntı
+          </button>
+        </blockquote>
+      </div>
     </div>
   );
 }
